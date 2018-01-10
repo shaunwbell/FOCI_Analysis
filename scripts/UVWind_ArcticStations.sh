1bb8814629166f50be70b5e54cefdbf956866c5c
shaunwbell <shaun.bell@noaa.gov>
2018-01-10 14:46:10 -0800

    update

--- scripts/UVWind_ArcticStations.sh
+++ scripts/UVWind_ArcticStations.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-data_dir="/Users/bell/Data_Local/Reanalysis_Files/NARR/daily/"
-prog_dir="/Users/bell/Programs/Python/FOCI_Analysis/"
+data_dir="/Volumes/WDC_internal//Users/bell/Data_Local/Reanalysis_Files/NARR/daily/"
+prog_dir="/Volumes/WDC_internal//Users/bell/Programs/Python/FOCI_Analysis/"
 
 
-python NARR_daily_WindsSFCtemp_Station.py C1 70.836 163.124 2010 2016
-python NARR_daily_WindsSFCtemp_Station.py C2 71.230 164.105 2010 2016
-python NARR_daily_WindsSFCtemp_Station.py C3 71.825 165.975 2010 2016
-python NARR_daily_WindsSFCtemp_Station.py C4 71.041 160.517 2010 2016
-python NARR_daily_WindsSFCtemp_Station.py C5 71.202 158.005 2010 2016
-python NARR_daily_WindsSFCtemp_Station.py C6 71.772 161.882 2010 2016
-python NARR_daily_WindsSFCtemp_Station.py C7 72.424 161.604 2010 2016
-python NARR_daily_WindsSFCtemp_Station.py C8 72.586 161.215 2010 2016
-python NARR_daily_WindsSFCtemp_Station.py C9 72.467 156.550 2010 2016
+#python NARR_daily_WindsSFCtemp_Station.py C1 70.836 163.124 2010 2016
+#python NARR_daily_WindsSFCtemp_Station.py C2 71.230 164.105 2010 2016
+#python NARR_daily_WindsSFCtemp_Station.py C3 71.825 165.975 2010 2016
+#python NARR_daily_WindsSFCtemp_Station.py C4 71.041 160.517 2010 2016
+#python NARR_daily_WindsSFCtemp_Station.py C5 71.202 158.005 2010 2016
+#python NARR_daily_WindsSFCtemp_Station.py C6 71.772 161.882 2010 2016
+#python NARR_daily_WindsSFCtemp_Station.py C7 72.424 161.604 2010 2016
+#python NARR_daily_WindsSFCtemp_Station.py C8 72.586 161.215 2010 2016
+python NARR_daily_WindsSFCtemp_Station.py C9 72.467 156.550 2014 2017
 #python NARR_daily_WindsSFCtemp_Station.py C10 -
 #python NARR_daily_WindsSFCtemp_Station.py C11 -
-python NARR_daily_WindsSFCtemp_Station.py C12 67.910 168.198 2010 2016
+#python NARR_daily_WindsSFCtemp_Station.py C12 67.910 168.198 2010 2016
