66eb56a6a9e7443942a233895bd06dc1e47596bc
Shaun Bell <shaun.bell@noaa.gov>
2020-05-26 12:49:22 -0700

    Update NARR_BeringStations.sh

--- scripts/NARR_BeringStations.sh
+++ scripts/NARR_BeringStations.sh
@@ -6,12 +6,13 @@ prog_dir="/Users/bell/Programs/Python/FOCI_Analysis/"
 
 #v M2 56.87 164.05 2005 2017 
 #python NARR_daily_WindsSFCtemp_Station.py M8 62.19 174.689 1980 2018 --DataPath ${data_dir}
-#python NARR_daily_WindsSFCtemp_Station.py M8 62.19 174.689 1980 2019 --DataPath ${data_dir}
+python NARR_daily_WindsSFCtemp_Station.py M8 62.19 174.689 1980 2019 --DataPath ${data_dir}
+python NARR_daily_WindsSFCtemp_Station.py M5 59.91 174.680 1980 2019 --DataPath ${data_dir}
 #python NARR_3hr_WindsSFCtemp_Station.py M8 62.19 174.689 2018 2019 --DataPath ${data_dir} 
 
 ## Arctic
 #python NARR_daily_WindsSFCtemp_Station.py C2 71.230 164.105 2018 2018 --DataPath ${data_dir}
-python NARR_daily_WindsSFCtemp_Station.py C2 71.230 164.105 2018 2019 --DataPath ${data_dir}
+#python NARR_daily_WindsSFCtemp_Station.py C2 71.230 164.105 2018 2019 --DataPath ${data_dir}
 #python NARR_daily_WindsSFCtemp_Station.py C9 72.467 156.550  2019 2019 --DataPath ${data_dir}
 
 ### GOA Stations
