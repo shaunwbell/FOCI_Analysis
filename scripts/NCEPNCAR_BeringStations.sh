dde4a5f0badd76fe5719575d5c138813ab156b70
Shaun Bell <shaun.bell@noaa.gov>
2021-01-06 09:12:28 -0800

    general update

--- scripts/NCEPNCAR_BeringStations.sh
+++ scripts/NCEPNCAR_BeringStations.sh
@@ -7,10 +7,15 @@ prog_dir="/Users/bell/Programs/Python/FOCI_Analysis/"
 #v M2 56.87 164.05 2005 2017 
 #python NARR_daily_WindsSFCtemp_Station.py M8 62.19 174.689 1980 2018 --DataPath ${data_dir}
 #python NARR_daily_WindsTemp_atheight.py M8 62.19 174.689 1980 2018 --DataPath ${data_dir}
-python NCEP_WindsSFCtemp_Station.py M8 62.19 174.689 2019 2019 --DataPath ${data_dir} -plot -sep
+#python NCEP_WindsSFCtemp_Station.py M8 62.19 174.689 2019 2019 --DataPath ${data_dir} -plot -sep
 
 ## Arctic
 #python NARR_daily_WindsSFCtemp_Station.py C2 71.230 164.105 2018 2018 --DataPath ${data_dir}
 
 ### GOA Stations
 #python NARR_3hr_WindsSFCtemp_Station.py Shumagin 55.0 160.5 2015 2017 --DataPath ${data_dir} -sm
+
+### Popups
+python NCEP_WindsSFCtemp_Station.py PUF_70010 60.91820 177.71234 2020 2020 --DataPath ${data_dir} -sep
+#python NCEP_WindsSFCtemp_Station.py PUF_77010 59.94237 175.48601 2020 2020 --DataPath ${data_dir} 
+
--- scripts/UVWind_ArcticStations.sh
+++ scripts/UVWind_ArcticStations.sh
@@ -5,7 +5,7 @@ prog_dir="/Users/bell/Programs/Python/FOCI_Analysis/"
 
 
 #python NARR_daily_WindsSFCtemp_Station.py C1 70.836 163.124 2010 2016
-#python NARR_daily_WindsSFCtemp_Station.py C2 71.230 164.105 2016 2019 --DataPath $data_dir
+python NARR_daily_WindsSFCtemp_Station.py C2 71.230 164.105 2010 2020 --DataPath $data_dir
 #python NARR_daily_WindsSFCtemp_Station.py C3 71.825 165.975 2010 2016
 #python NARR_daily_WindsSFCtemp_Station.py C4 71.041 160.517 2010 2016
 #python NARR_daily_WindsSFCtemp_Station.py C5 71.202 158.005 2010 2016
@@ -15,10 +15,10 @@ prog_dir="/Users/bell/Programs/Python/FOCI_Analysis/"
 #python NARR_3hr_WindsSFCtemp_Station.py C9 72.467 156.550 2014 2017 --DataPath $data_dir
 #python NARR_daily_WindsSFCtemp_Station.py C10 -
 #python NARR_daily_WindsSFCtemp_Station.py C11 -
-#python NARR_daily_WindsSFCtemp_Station.py C12 67.910 168.198 2010 2016
+python NARR_daily_WindsSFCtemp_Station.py C12 67.910 168.198 2010 2020 --DataPath $data_dir
 #python NARR_3hr_WindsSFCtemp_Station.py EARCTICSite 71.5 154.0 2014 2017 --DataPath $data_dir
 #python NARR_3hr_WindsSFCtemp_Station.py WARCTICSite 75.0 170.0 2014 2017 --DataPath $data_dir
 
 
 #anom dump
-python NARR_daily_WindsSFCtemp_StationAnomdump.py C2 71.230 164.105 2016 2016 --DataPath $data_dir
+#python NARR_daily_WindsSFCtemp_StationAnomdump.py C2 71.230 164.105 2016 2016 --DataPath $data_dir
