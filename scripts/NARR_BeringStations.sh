11abf79635eb8c02f51fa5c5212bd09e08dbb346
Shaun Bell <shaun.bell@noaa.gov>
2022-02-08 09:53:22 -0800

    narr data

--- scripts/NARR_BeringStations.sh
+++ scripts/NARR_BeringStations.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 data_dir="/Volumes/MobileSSD/in_and_outbox/data_sets/reanalyis_data/NARR/3hr/"
+data_dir="/Users/bell/Downloads/NARR/"
 prog_dir="/Users/bell/Programs/Python/FOCI_Analysis/"
 
 
@@ -11,7 +12,7 @@ prog_dir="/Users/bell/Programs/Python/FOCI_Analysis/"
 #python NARR_3hr_WindsSFCtemp_Station.py M8 62.19 174.689 2018 2019 --DataPath ${data_dir} 
 
 ## Arctic
-python NARR_daily_WindsSFCtemp_Station.py C2 71.230 164.105 2015 2020 --DataPath ${data_dir}
+python NARR_3hr_WindsSFCtemp_Station.py C2 71.230 164.105 2012 2012 --DataPath ${data_dir}
 #python NARR_daily_WindsSFCtemp_Station.py C2 71.230 164.105 2018 2019 --DataPath ${data_dir}
 #python NARR_daily_WindsSFCtemp_Station.py C9 72.467 156.550  2019 2019 --DataPath ${data_dir}
 
